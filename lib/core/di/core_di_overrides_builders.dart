bffb4a49479a664b4b0ee721f106cf032889ae85
Bobur Irgashev <bobur.i@iman.uz>
2025-09-29 02:48:52 +0500

    minor fixes

--- lib/core/di/core_di_overrides_builders.dart
+++ lib/core/di/core_di_overrides_builders.dart
@@ -3,12 +3,14 @@ import 'package:core/exports/di.dart';
 import 'package:core/providers.dart';
 
 // Project imports:
+import 'package:weather_app/core/crashlytics_logger/crashlytics_logger.dart';
 import 'package:weather_app/core/error_handler/error_handler_cubit.dart';
 import 'package:weather_app/core/providers.dart';
 import 'package:weather_app/core/storage/locale_storage/locale_storage.dart';
 import 'package:weather_app/router/app_router.dart';
 
 List<Override> buildCoreDiOverrodes() => [
+  loggerProvider.overrideWith((ref) => const CrashlyticsLogger()),
   appRouterProvider.overrideWith((ref) => AppRouter()),
   localeStorageProvider.overrideWith(
     (ref) =>
@@ -16,17 +18,6 @@ List<Override> buildCoreDiOverrodes() => [
   ),
   errorHandlerCubitProvider.overrideWith((ref) {
     final errorHandler = ErrorHandlerCubit();
-    // final logger = ref.watch(loggerProvider);
-
-    // BlocErrorDelegate.instance.startListen((error, stackTrace) {
-    //   final handledError = error is AppFailure
-    //       ? error
-    //       : UnhandledFailure(error, stackTrace);
-
-    //   logger.error("BlocErrorDelegate error", error, stackTrace);
-    //   errorHandler.handle(handledError);
-    // });
-
     return errorHandler;
   }),
 ];
